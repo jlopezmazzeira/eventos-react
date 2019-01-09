7815fbc9a6ab597ac21c6a2b84b8279e611ea8ea
jlopezmazzeira <jlopezmazzeira@gmail.com>
2019-01-09 17:43:18 -0300

    WIP

---
+++ src/components/Evento.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Evento extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default Evento;
---
+++ src/components/Formulario.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Formulario extends Component {
+  categoriaRef = React.createRef();
+  nombreRef = React.createRef();
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default Formulario;
